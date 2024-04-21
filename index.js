882e468ea5b2afae1befde85f86095da27144601
pirince <pliyo@ivytech.edu>
2024-04-20 21:54:39 -0500

    update the index js and index ejs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express");
  const path = require('path')
 
@@ -8,13 +9,20 @@ const port = 3000;
 //views Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-//wildderware for static files
+//srt EJS as temple engine for static files
 app.use(express.static("public"));
+// Parse Fson and url-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 app.get("/", (req, res) => {
   res.render("pages/index");
 });
+app.post('courses',(req, res)=>{
+
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
